45c0aa4e66e5f7176c15b5b62a5ab55c008bf8be
HanYu <moonlight612311521@gmail.com>
2015-10-31 00:13:25 +0800

    use mysql instead of sqlite

--- LorR/settings.py
+++ LorR/settings.py
@@ -112,8 +112,12 @@ SOCIAL_AUTH_PIPELINE = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hackathon',
+        'USER': 'root',
+        'PASSWORD' : '',
+        'HOST' : '',
+        'PORT' : '',
     }
 }
 
