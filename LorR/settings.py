266e901fbe61b83cadbc09a14ebe985ac75d2103
Kaizhi <mengk@garena.com>
2015-10-30 18:05:29 +0800

    update ui

--- LorR/settings.py
+++ LorR/settings.py
@@ -113,4 +113,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = '/Users/kaizhimeng/personal/static/'
 STATIC_URL = '/static/'
--- hackathon/static/app.js
+++ hackathon/static/app.js
@@ -1,6 +1,6 @@
 'use static';
 
-var app = angular.module('LoR', ['ngRoute']);
+var app = angular.module('LoR', ['ngRoute', 'gajus.swing']);
 
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
@@ -21,6 +21,42 @@ app.controller('HomeController', function($scope) {
 	$scope.user = {
 		name: 'test'
 	};
+
+	$scope.cards = [{
+			body: 'this is the first question',
+			answered: true
+		}, {
+			body: 'this is the second question',
+			answered: false
+		}, {
+			body: 'this is the third question',
+			answered: false
+		}];
+
+	$scope.remove = function(index, obj) {
+		console.log($scope.cards[index]);
+		$scope.cards[index].answered = true;
+        $scope.cards.splice(index, 1);
+	};
+
+	$scope.throwoutleft = function(event, obj) {
+		// console.log(event);
+		// console.log(obj);
+	};
+
+	$scope.throwoutright = function(event, obj) {
+
+	};
+
+    $scope.options = {
+        throwOutConfidence: function (offset, elementWidth) {
+            return Math.min(Math.abs(offset) / 160, 1);
+        },
+        isThrowOut: function (offset, elementWidth, throwOutConfidence) {
+            console.log('isThrowOut', offset, elementWidth, throwOutConfidence);
+            return throwOutConfidence === 1;
+        }
+    };
 });
 
 app.controller('MeController', function($scope) {
