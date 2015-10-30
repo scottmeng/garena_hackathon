80d575660a10ed7b54c88a315e9b11f448a2ddc3
Kaizhi <mengk@garena.com>
2015-10-30 18:13:51 +0800

    update static root

--- LorR/settings.py
+++ LorR/settings.py
@@ -135,5 +135,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = '/Users/kaizhimeng/personal/static/'
+STATIC_ROOT = '../static/'
 STATIC_URL = '/static/'
