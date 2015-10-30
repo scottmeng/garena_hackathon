b07f6a1a8cf7ee9c559c21308f036909e9fcdf8a
naijia zheng <naijia@naijias-MacBook-Pro.local>
2015-10-30 11:28:43 +0800

    add three basic models

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-#class Questions(models.Model):
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=128)
+    class Meta:
+        db_table = 'answer_tab'
+
+class Question(models.Model):
+    question = models.CharField()
+    answer_left = models.ForeignKey(Answer, on_delete=models.PROTECT)
+    answer_right = models.ForeignKey(Answer, on_delete=models.PROTECT)
+    create_time = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'question_tab'
+
+class AnswerHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'answer_history_tab'
