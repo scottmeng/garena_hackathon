c7f12de537a237e1b4be9cb9f18b8a7270937d7a
laizhouxia <znj472982642@gmail.com>
2015-10-30 12:41:02 +0800

    edit model

--- hackathon/models.py
+++ hackathon/models.py
@@ -4,29 +4,35 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Answer(models.Model):
-    answer = models.CharField(max_length=128)
-    enable = models.BooleanField(default=True)
-    class Meta:
-        db_table = 'answer_tab'
-
 class Question(models.Model):
     question = models.CharField(max_length=2555)
-    left = models.ForeignKey(Answer, on_delete=models.PROTECT, related_name='left')
-    right = models.ForeignKey(Answer, on_delete=models.PROTECT, related_name='right')
+    left = models.CharField(max_length=128)
+    right = models.CharField(max_length=128)
     view_count = models.PositiveIntegerField(default=0)
     answer_count = models.PositiveIntegerField(default=0)
-    ignore_count = models.PositiveIntegerField(default=0)
-    discard_count = models.PositiveIntegerField(default=0)
+    report_count = models.PositiveIntegerField(default=0)
+    skip_count = models.PositiveIntegerField(default=0)
     create_time = models.DateTimeField(auto_now_add=True)
     enable = models.BooleanField(default=True)
     class Meta:
         db_table = 'question_tab'
 
 class AnswerHistory(models.Model):
+    NO_ANSWER = 0
+    LEFT = 1
+    RIGHT = 2
+    SKIP = 3
+    REPORT = 4
+    ANSWER_CHOICES = ((NO_ANSWER, 'No_Answer'),
+                    (LEFT, 'Left'),
+                    (RIGHT, 'Right'),
+                    (SKIP, 'Skip'),
+                    (REPORT, 'Report')
+                    )
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
-    answer = models.ForeignKey(Answer, on_delete=models.PROTECT)
+    answer = models.PositiveIntegerField(choices=ANSWER_CHOICES,default=NO_ANSWER)
     enable = models.BooleanField(default=True)
+    
     class Meta:
         db_table = 'answer_history_tab'
